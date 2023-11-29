77ef27176b29a54bb0e543f2c318eed501cf072c
ved-thakre <vedantthakre7@gmail.com>
2023-11-29 16:57:49 +0530

    cors added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import jsonRouter from './Routes/jsonRoutes.js'
-
+import cors from "cors";
 import dotenv from 'dotenv';
 
 export const app = express();
@@ -9,6 +9,11 @@ dotenv.config();
 // Middleware to parse JSON data
 app.use(express.json());
 app.use("/api/json", jsonRouter);
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  methods: ["GET", "PUT", "POST", "DELETE"],
+  credentials: true,
+}));
 
 app.get("/", (req, res) =>{
     res.send("Server is Live");
